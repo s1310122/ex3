e5d46d68e273e055fa0530c1766bade53ea8d02c
s1310122 <s1310122@u-aizu.ac.jp>
2024-07-03 15:12:28 +0900

    User name added

--- coin.c
+++ coin.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 
 int main(){
+     char name[100];
+    printf("Who are you?\n>");
+    scanf("%s", name);
+    printf("Hello,%s\n", name);
+    
     int count_H=0, count_T=0;
     printf("Tossing a coin...\n");
     for(int i=1;i<4;i++){
